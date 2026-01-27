ab57d8bfb8d17e1b3c6466aaa5a95f4759946e93
Subhadeep Sarkar <subhadeep1007@gmail.com>
2026-01-28 00:52:51 +0530

    firebase hosting deployed

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,16 +7,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDFPpuWwMsw3gzsBpxjpwmO7sscEv88xAw",
-  authDomain: "pokes-on-you-website.firebaseapp.com",
-  projectId: "pokes-on-you-website",
-  storageBucket: "pokes-on-you-website.firebasestorage.app",
-  messagingSenderId: "522172597703",
-  appId: "1:522172597703:web:fdf3a3d0021270a88d45e2",
-  measurementId: "G-Y2LQ22D5GQ"
+  apiKey: "AIzaSyCHg0OMfE8jXXAyDyLWnbr2kOgXxCPzyJg",
+  authDomain: "pokesonyou-88c9a.firebaseapp.com",
+  projectId: "pokesonyou-88c9a",
+  storageBucket: "pokesonyou-88c9a.firebasestorage.app",
+  messagingSenderId: "368841700177",
+  appId: "1:368841700177:web:ae6b3ce527d7d1c68b1970",
+  measurementId: "G-0HE0RQBHMQ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const db = getFirestore(app);
